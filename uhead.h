5d2e12f10a8f00c9163d29e969feb73fba2dabae
zwq7426 <55750522+zwq7426@users.noreply.github.com>
2019-09-24 22:18:46 +0800

    Create uhead.h

---
+++ uhead.h
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+/*struct arg{
+	int sum;
+	float ave;
+};*/
+
+void show(int*,int);
+void urand(int*,int);
+int sum(int*,int);
+double ave(int,int);
+void uwrite(const char *,int *,int);
+void uread(const char *,int *,int);
+/*struct arg uoperate(int*,int)*/
